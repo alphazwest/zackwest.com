65cdd7dc8d2c41991c26e94f3cc8e93cf88fa1df
alphazwest <alphazwest@gmail.com>
2023-01-09 23:30:20 -0500

    fixed mobile nav menu not disappearing on element click.

--- assets/js/az.js
+++ assets/js/az.js
@@ -8,6 +8,16 @@ let mm = document.querySelector('.mobile-nav');
 // select the hamburger menu, add event listener to display menu.
 // onclick doesn't work here for some reason.
 let mmOpen = document.querySelector('#mobileNavToggle');
+
+// any click to a mobile nav element will hide the menu
+// but reveal when an element is clicked.
+let mmClick = document.querySelectorAll('.mobile-nav li a');
+mmClick.forEach( (item) => {
+    item.addEventListener('click', function(){
+        mm.classList.toggle('expanded');
+    })
+})
+
 mmOpen.addEventListener('click', function(){
     mm.classList.toggle('expanded');
 })
